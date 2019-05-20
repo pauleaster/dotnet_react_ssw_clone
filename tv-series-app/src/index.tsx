5ab9008a3890c487150c36d32d9e688944560d53
Partho P. Das <parthopdas@live.com>
2019-05-20 12:10:29 +1000

    Ch 03 - TSX

--- tv-series-app/src/index.tsx
+++ tv-series-app/src/index.tsx
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const greeting = React.createElement('h1', {}, 'Hello World!');
+const getCurrentDate = () => {
+    const date = new Date();
+    return date.toDateString();
+}
+
+const greeting = <h1>Hello World! Current date: {getCurrentDate()}</h1>;
+
 ReactDOM.render(greeting, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
