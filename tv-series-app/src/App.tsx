de63083a8e78c040728018aedf22e1c78365dc39
Partho P. Das <parthopdas@live.com>
2019-05-20 12:20:55 +1000

    Ch 04 - Components

--- tv-series-app/src/App.tsx
+++ tv-series-app/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const Intro = (props: any) => (
+  <p className='App-intro'>
+    Our first functional component.
+  </p>
+);
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='App-title'>TV Series List</h1>
       </header>
+      <Intro />
     </div>
   );
 }
