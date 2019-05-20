9cb18e43215ab2ba0cd70942253fc2cfafb7cf0e
Partho P. Das <parthopdas@live.com>
2019-05-20 12:07:27 +1000

    Ch 02 - React Elements

--- tv-series-app/src/index.tsx
+++ tv-series-app/src/index.tsx
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const greeting = React.createElement('h1', {}, 'Hello World!');
+ReactDOM.render(greeting, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
