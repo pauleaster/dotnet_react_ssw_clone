22c8752f2c1476b7554d4ff4012198c7619ef95b
Paul Easter <paul.john.easter@gmail.com>
2023-05-22 23:31:17 +1000

    Updated to .NET 6 and everything builds but still getting an exception

--- TVSeriesApp/Startup.cs
+++ TVSeriesApp/Startup.cs
@@ -20,7 +20,7 @@ namespace TVSeriesApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
