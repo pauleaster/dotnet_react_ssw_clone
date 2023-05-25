31736f3fd6ea4617e9323b5c2ffedd0829637254
Paul Easter <paul.john.easter@gmail.com>
2023-05-25 11:30:01 +1000

    React code now compiles and builds

--- TVSeriesApp/ClientApp/src/components/Main/index.tsx
+++ TVSeriesApp/ClientApp/src/components/Main/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Series } from '../../containers/Series';
-import { SingleSeries } from '../../containers/SingleSeries';
+import SingleSeries from '../../containers/SingleSeries';
 
 export function Main(): any {
   return (
