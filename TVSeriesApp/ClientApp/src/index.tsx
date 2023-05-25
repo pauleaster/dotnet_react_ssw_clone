d4ea2b66bcb0600ccb6047915c22339e133def39
Paul Easter <paul.john.easter@gmail.com>
2023-05-25 18:58:17 +1000

    Added error boundary around app

--- TVSeriesApp/ClientApp/src/index.tsx
+++ TVSeriesApp/ClientApp/src/index.tsx
@@ -5,16 +5,51 @@ import * as Router from 'react-router-dom';
 import * as Containers from './containers';
 import * as serviceWorker from './serviceWorker';
 
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  static getDerivedStateFromError(_error: Error): ErrorBoundaryState | null {
+    return { hasError: true };
+  }
+
+  render(): React.ReactNode {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  componentDidCatch(error: Error, _errorInfo: React.ErrorInfo): void {
+    console.error(error);
+  }
+}
+
 const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
   root.render(
-    <Router.BrowserRouter>
-      <Containers.App />
-    </Router.BrowserRouter>,
+    <ErrorBoundary>
+      <Router.BrowserRouter>
+        <Containers.App />
+      </Router.BrowserRouter>
+    </ErrorBoundary>,
   );
 }
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
