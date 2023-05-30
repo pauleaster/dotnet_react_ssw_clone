05a5918253796ba1569d4c2baa4d83c1ee18e04c
Paul Easter <paul.john.easter@gmail.com>
2023-05-30 11:36:29 +1000

    Refactored Program.cs to remove  Startup.cs

--- TVSeriesApp/Program.cs
+++ TVSeriesApp/Program.cs
@@ -4,9 +4,14 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 
 namespace TVSeriesApp
 {
@@ -19,6 +24,50 @@ namespace TVSeriesApp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                 .ConfigureServices((hostContext, services) =>
+                 {
+                     services.AddMvc(options => options.EnableEndpointRouting = false);
+
+                     // In production, the React files will be served from this directory
+                     services.AddSpaStaticFiles(configuration =>
+                     {
+                         configuration.RootPath = "ClientApp/build";
+                     });
+                 })
+                .Configure((hostContext, app) =>
+                {
+                    var env = hostContext.HostingEnvironment;
+                    if (env.IsDevelopment())
+                    {
+                        app.UseDeveloperExceptionPage();
+                    }
+                    else
+                    {
+                        app.UseExceptionHandler("/Error");
+                        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                        app.UseHsts();
+                    }
+
+                    app.UseHttpsRedirection();
+                    app.UseStaticFiles();
+                    app.UseSpaStaticFiles();
+
+                    app.UseMvc(routes =>
+                    {
+                        routes.MapRoute(
+                            name: "default",
+                            template: "{controller}/{action=Index}/{id?}");
+                    });
+
+                    app.UseSpa(spa =>
+                    {
+                        spa.Options.SourcePath = "ClientApp";
+
+                        if (env.IsDevelopment())
+                        {
+                            spa.UseReactDevelopmentServer(npmScript: "start");
+                        }
+                    });
+                });
     }
 }
